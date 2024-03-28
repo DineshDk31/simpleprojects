80928869e3110b41bb5f6410c82a4003021d3b1d
DineshDk31 <114717176+DineshDk31@users.noreply.github.com>
2024-03-28 11:46:58 +0530

    Add files via upload

---
+++ Portfolio/portofolio.js
@@ -0,0 +1,25 @@
+let menu=document.getElementById('menuicon')
+
+menu.addEventListener('click',function (){
+    let rnav=document.getElementById('rightnavigation')
+    
+    rnav.style.visibility='visible'
+    
+    
+    let home=document.getElementById('home')
+    home.style.display='block'
+   
+    
+})
+
+let closeicon=document.getElementById('closeicon')
+closeicon.addEventListener('click',function (){
+    let rnav=document.getElementById('rightnavigation')
+    
+    rnav.style.visibility='hidden';
+    
+    let home=document.getElementById('home')
+    home.style.display='none';
+     
+})
+
